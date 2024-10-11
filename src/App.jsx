167c8bc1e5fcd3ef04c6fc6b0fb8eb75e87b2c8f
Ramy Kaddouri <myramkadd@gmail.com>
2024-10-11 17:54:45 -0400

    style fixes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <h1> Todo App </h1>
 
       <div id="task-bar">
-        <input className="task-input" type="text" />
+        <input className="task-input" type="text" placeholder='Enter a task here...' />
         <button className="task-button">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="plus-icn">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
