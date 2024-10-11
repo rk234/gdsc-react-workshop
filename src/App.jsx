cf5b8e7755115e63caab8842d8e3a4622d91ada0
Ramy Kaddouri <myramkadd@gmail.com>
2024-10-11 17:46:55 -0400

    style done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <div className="content">
@@ -12,13 +12,18 @@ function App() {
 
       <div id="task-bar">
         <input className="task-input" type="text" />
-        <button>Add Task</button>
+        <button className="task-button">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="plus-icn">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+          Add Task
+        </button>
       </div>
       <div className="task-list">
 
       </div>
       <div className="task-list">
-      
+
       </div>
     </div>
   )
