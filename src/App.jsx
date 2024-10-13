5edfec735b2a7f182f1ac9101c62f83afabb5f6d
Ramy Kaddouri <myramkadd@gmail.com>
2024-10-13 19:59:02 -0400

    small edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import Task from './components/Task';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState("");
+  const [newTask, setNewTask] = useState("");
 
   const handleClick = () => {
     setTasks([
       ...tasks,
-      { value: task, completed: false }
+      { value: newTask, completed: false }
     ])
-    setTask("");
+    setNewTask("");
   }
 
   const handleCheck = (id, complete) => {
@@ -27,8 +27,14 @@ function App() {
       <h1> Todo App </h1>
 
       <div id="task-bar">
-        <input value={task} onChange={(e) => setTask(e.target.value)} className="task-input" type="text" placeholder='Enter a task here...' />
-        <button disabled={task.length == 0} onClick={handleClick} className="task-button">
+        <input
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          className="task-input"
+          type="text"
+          placeholder='Enter a task here...' />
+
+        <button disabled={newTask.length == 0} onClick={handleClick} className="task-button">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="plus-icn">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
           </svg>
